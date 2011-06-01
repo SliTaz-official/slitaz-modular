ab41108253dd4a60f0b1cd02a18f5adc25b47cf8
Christopher Rogers <slaxemulator@gmail.com>
2011-06-01 09:17:31 +0000

    Added full support for cook.slitaz.org.

--- initramfs/etc/init.d/local.sh
+++ initramfs/etc/init.d/local.sh
@@ -5,7 +5,6 @@
 #
 . /etc/init.d/rc.functions
 . /etc/slitaz/slitaz.conf
-
 echo "Starting local startup commands... "
 
 [ -d /etc/pango ] || mkdir -p /etc/pango
@@ -14,4 +13,10 @@ echo "Starting local startup commands... "
 [ -x /usr/bin/gdk-pixbuf-query-loaders ] && /usr/bin/gdk-pixbuf-query-loaders > /etc/gtk-2.0/gdk-pixbuf.loaders
 [ -x /usr/bin/update-mime-database ] && update-mime-database /usr/share/mime
 
+if [ -f /etc/local-mirror.conf ]; then
+	. /etc/local-mirror.conf
+	if [ ! $(grep -l "$PKGDIR" $LOCALSTATE/mirror) ]; then
+		[ -d $PKGDIR ] && echo "$PKGDIR" > $LOCALSTATE/mirror
+	fi
+fi
 #[ -x /usr/bin/setup-live ] && /usr/bin/setup-live
